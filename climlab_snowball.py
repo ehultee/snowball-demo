a95bd3074ad94cf77728b0a0132521f161d284aa
Lizz Ultee [air] <ehultee@umich.edu>
2020-11-30 11:25:29 -0500

    Graphics and fig formatting
    Re-format subplots in `state_plot`.  Add motivating figures for intro and section 4 of notebook.

--- climlab_snowball.py
+++ climlab_snowball.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def state_plot(model, figsize=(8,12), show=True, ice_temp=True):
+def state_plot(model, figsize=(12,5), show=True, ice_temp=True):
     """Plot the temperature and albedo at the current state of the model.
     Shade the current ice line in grey."""
     templimits = -30,35
@@ -22,14 +22,14 @@ def state_plot(model, figsize=(8,12), show=True, ice_temp=True):
     
     fig = plt.figure(figsize=figsize)
     
-    ax1 = fig.add_subplot(2,1,1)
+    ax1 = fig.add_subplot(1,2,1)
     ax1.plot(model.lat, Ts)
     ax1.set(xlim=latlimits, ylim=templimits,
-           ylabel='Temperature [deg C]', xticks=lat_ticks)
+           ylabel='Temperature [deg C]', xlabel='Latitude', xticks=lat_ticks)
     ax1.fill_between(model.lat, Ts, y2=Tf, where=Ts<Tf, color='LightGrey', alpha=0.5)
     ax1.grid()
     
-    ax2 = fig.add_subplot(2,1,2)
+    ax2 = fig.add_subplot(1,2,2)
     icerect1 = patches.Rectangle((latlimits[0], 0), width=model.icelat[0]-latlimits[0], height=1,
                             color='LightGrey', alpha=0.5)
     icerect2 = patches.Rectangle((model.icelat[1], 0), width=latlimits[1]-model.icelat[1], height=1,
@@ -37,7 +37,6 @@ def state_plot(model, figsize=(8,12), show=True, ice_temp=True):
     ax2.add_patch(icerect1)
     ax2.add_patch(icerect2)
     ax2.plot(model.lat, model.albedo)
-    ax2.set_xlim(latlimits)
-    ax2.set_ylim(alimits)
-    ax2.set_ylabel('Albedo')
+    ax2.set(xlim=latlimits, ylim=alimits,
+           ylabel='Albedo', xlabel='Latitude', xticks=lat_ticks)
     ax2.grid()
